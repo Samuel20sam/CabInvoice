d6766ca4a20b02bb7f252cd88538a24d6d2246a1
Samuel_Elijah <samuel20sam@gmail.com>
2022-07-06 08:04:32 +0530

    Adding multiple rides

--- src/test/java/com/bridgelabz/cabinvoice/CabInvoiceTest.java
+++ src/test/java/com/bridgelabz/cabinvoice/CabInvoiceTest.java
@@ -13,12 +13,20 @@ public class CabInvoiceTest {
         Assertions.assertEquals(25.0, fare);
     }
 
-   @Test
-    public void givenLessDistanceAndFare_ShouldReturnMinFare(){
+    @Test
+    public void givenLessDistanceAndFare_ShouldReturnMinFare() {
         CabInvoice inVoiceGenerator = new CabInvoice();
         double distance = 0.1;
         int time = 1;
         double fare = inVoiceGenerator.calculateFare(distance, time);
         Assertions.assertEquals(5.0, fare);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+        Ride[] rides = {new Ride(2.0, 5), new Ride(0.1, 1), new Ride(20.5, 35)};
+        CabInvoice inVoiceGenerator = new CabInvoice();
+        double fare = inVoiceGenerator.calculateFare(rides);
+        Assertions.assertEquals(270.0, fare);
     }
 }
