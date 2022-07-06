af02221acef49fac1d1d3c4f334e1a78895830a8
Samuel_Elijah <samuel20sam@gmail.com>
2022-07-06 07:45:33 +0530

    Adding: generating invoice

---
+++ src/main/java/com/bridgelabz/cabinvoice/CabInvoice.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.cabinvoice;
+
+public class CabInvoice {
+    final int COST_PER_MIN=1;
+    final double MIN_COST_PER_KM=10;
+    final double MIN_FARE=5;
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MIN_COST_PER_KM + time * COST_PER_MIN;
+        return Math.max(totalFare, MIN_FARE);
+    }
+}
